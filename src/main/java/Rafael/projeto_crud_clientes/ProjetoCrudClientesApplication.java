7b0d23428862a2378651b4eded9a6047db2c8fd9
Rafpazzz <rafael_mourapaz@hotmail.com>
2026-01-26 19:59:00 -0300

    Primeiro commit: Estrutura inicial do Projeto

---
+++ src/main/java/Rafael/projeto_crud_clientes/ProjetoCrudClientesApplication.java
@@ -0,0 +1,13 @@
+package Rafael.projeto_crud_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCrudClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Rafael/projeto_crud_clientes/ProjetoCrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package Rafael.projeto_crud_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
