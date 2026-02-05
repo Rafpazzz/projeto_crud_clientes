907dd84384af20d44268297953982bc67795fff0
Rafael Paz <rafael_mourapaz@hotmail.com>
2026-02-04 23:58:45 -0300

    .

---
+++ src/main/java/Rafael/projeto_crud_clientes/controller/UserController_RoleUser.java
@@ -0,0 +1,11 @@
+package Rafael.projeto_crud_clientes.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cadastro")
+@requiredArgsConstructor
+public class UserController_RoleUser {
+    
+}
